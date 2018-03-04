f1adb1e5f2575c90a450b03ba579989c62ce0553
Ilya Portnov <portnov@bk.ru>
2018-03-04 18:54:36 +0500

    Buggy.

--- src/Main.hs
+++ src/Main.hs
@@ -4,20 +4,36 @@ module Main where
 import Control.Monad (forM_)
 import Text.Printf
 import Graphics.Gloss
+import Graphics.Gloss.Interface.IO.Game
+import Graphics.Gloss.Data.ViewPort
+import Graphics.Gloss.Data.ViewState
 
 import Types
 import Circles
 
+data Game = Game {
+    gField :: Field,
+    gView :: ViewState
+  }
+
+handler :: Event -> Game -> Game
+handler (EventKey (Char '1') Up _ _) g = g {gField = rotate0 (gField g)}
+handler (EventKey (Char '2') Up _ _) g = g {gField = rotate1 (gField g)}
+handler (EventKey (Char '3') Up _ _) g = g {gField = rotate2 (gField g)}
+handler evt g = g {gView = updateViewStateWithEvent evt (gView g)}
+
+animation :: Float -> Game -> Game
+animation _ = id
+
+render :: Game -> Picture
+render g =
+  let viewPort = viewStateViewPort (gView g)
+  in  applyViewPortToPicture viewPort $ drawField (gField g)
+
 main :: IO ()
 main = do
   let mode = InWindow "Circles" (800, 600) (0,0)
-  let Field [c0, c1, c2] = initialField
-  -- display mode white $ drawCycle [] c1
-  let field = rotate2 initialField
+  let game = Game initialField viewStateInit
   print initialField
-  display mode white $ drawField field 
-  -- display mode white $ drawField initialField
-
---   forM_ allEquations $ \(c1,c2) ->
---     printf "%s = %s\n" (show c1) (show c2)
+  play mode white 100 game render handler animation
 
